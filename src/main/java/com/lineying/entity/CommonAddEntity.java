b89b245d82591163e904e194945f16f80540976f
Tony <chengkaizone@users.noreply.github.com>
2023-11-29 07:02:14 +0800

    添加setter/getter方法

--- src/main/java/com/lineying/entity/CommonAddEntity.java
+++ src/main/java/com/lineying/entity/CommonAddEntity.java
@@ -4,7 +4,32 @@ import lombok.Data;
 
 @Data
 public class CommonAddEntity {
-    private String column;
+
     private String table;
+    private String column;
     private String value;
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getColumn() {
+        return column;
+    }
+
+    public void setColumn(String column) {
+        this.column = column;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
--- src/main/java/com/lineying/entity/CommonUpdateEntity.java
+++ src/main/java/com/lineying/entity/CommonUpdateEntity.java
@@ -4,7 +4,32 @@ import lombok.Data;
 
 @Data
 public class CommonUpdateEntity {
+
+    private String table;
     private String where;
     private String set;
-    private String table;
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getWhere() {
+        return where;
+    }
+
+    public void setWhere(String where) {
+        this.where = where;
+    }
+
+    public String getSet() {
+        return set;
+    }
+
+    public void setSet(String set) {
+        this.set = set;
+    }
 }
