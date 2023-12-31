01b2c1591bd8334b0ca7edfc892ef68ef3594338
Tony <chengkaizone@users.noreply.github.com>
2023-12-31 22:13:45 +0800

    完善短信验证

--- src/main/java/com/lineying/sms/BaseSmsComponent.java
+++ src/main/java/com/lineying/sms/BaseSmsComponent.java
@@ -3,8 +3,6 @@ package com.lineying.sms;
 import com.lineying.common.CommonConstant;
 import com.tencentcloudapi.common.Credential;
 import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-import com.tencentcloudapi.common.profile.ClientProfile;
-import com.tencentcloudapi.common.profile.HttpProfile;
 import com.tencentcloudapi.common.profile.Region;
 import com.tencentcloudapi.sms.v20210111.SmsClient;
 import com.tencentcloudapi.sms.v20210111.models.SendSmsRequest;
@@ -12,39 +10,42 @@ import com.tencentcloudapi.sms.v20210111.models.SendSmsResponse;
 
 import java.util.logging.Logger;
 
-public class BaseSmsComponent {
+/**
+ * 消息发送
+ */
+public abstract class BaseSmsComponent {
 
     /**
      * 获取SmsClient客户端
      * @return
      */
     public SmsClient getClient() {
-
-        // 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey,此处还需注意密钥对的保密
-        // 密钥可前往https://console.cloud.tencent.com/cam/capi网站进行获取
         Credential cred = new Credential(CommonConstant.TENCENTCLOUD_SECRET_ID, CommonConstant.TENCENTCLOUD_SECRET_KEY);
-        // 实例化一个http选项，可选的，没有特殊需求可以跳过
-        HttpProfile httpProfile = new HttpProfile();
-        //这个setEndpoint可以省略的
-        httpProfile.setEndpoint("sms.tencentcloudapi.com");
-        // 实例化一个client选项，可选的，没有特殊需求可以跳过
-        ClientProfile clientProfile = new ClientProfile();
-        clientProfile.setHttpProfile(httpProfile);
-        // 实例化要请求产品的client对象,clientProfile是可选的
-        return new SmsClient(cred, Region.Shanghai.getValue(), clientProfile);
+        return new SmsClient(cred, Region.Shanghai.getValue());
     }
 
-    protected int sendCode(String phone, String code) {
-        // TODO
-        return 0;
-    }
+    /**
+     * 发送验证码
+     * @param phone
+     * @param code
+     * @return
+     */
+    protected abstract int sendCode(String phone, String code);
 
+    /**
+     * 发送验证码
+     * @param sdkAppId
+     * @param signName
+     * @param phone
+     * @param code
+     * @param templateCodeId
+     * @return
+     */
     public int sendCode(String sdkAppId, String signName, String phone, String code, String templateCodeId) {
-
-        // 返回的resp是一个SendSmsResponse的实例，与请求对象对应
         SendSmsResponse resp;
         try {
-            resp = getClient().SendSms(getReqTwo(sdkAppId, signName, phone, code, templateCodeId));  // 模板id是自己设置好的
+            SendSmsRequest req = makeRequest(sdkAppId, signName, phone, code, templateCodeId);
+            resp = getClient().SendSms(req);
             Logger.getGlobal().info(SendSmsResponse.toJsonString(resp));   // 把返回信息输入到日志中
         } catch (TencentCloudSDKException e) {
             e.printStackTrace();
@@ -62,7 +63,7 @@ public class BaseSmsComponent {
      * @param templateId  短息模板id
      * @return
      */
-    public static SendSmsRequest getReqTwo(String sdkAppId, String signName, String phone, String code, String templateId) {
+    public static SendSmsRequest makeRequest(String sdkAppId, String signName, String phone, String code, String templateId) {
         SendSmsRequest req = new SendSmsRequest();
         String targetPhone = phone;
 
@@ -70,14 +71,13 @@ public class BaseSmsComponent {
             targetPhone = "+86" + phone;
         }
         String[] phoneNumberSet = { targetPhone };
-        req.setSmsSdkAppId(sdkAppId); // 设置参数
+        req.setSmsSdkAppId(sdkAppId);
         req.setPhoneNumberSet(phoneNumberSet);
         req.setSignName(signName);
         req.setTemplateId(templateId);
-        //模板内容的参数有几个就设置几个，我这里是两个
         String[] templateParamSet = { code };
         req.setTemplateParamSet(templateParamSet);
-        return req;     // 返回请求参数内容
+        return req;
     }
 
 }
--- src/main/java/com/lineying/sms/MathCalcSmsComponent.java
+++ src/main/java/com/lineying/sms/MathCalcSmsComponent.java
@@ -9,21 +9,15 @@ public class MathCalcSmsComponent extends BaseSmsComponent {
     @Value("${sms.mathcalc.sdkAppId}")   // 注入参数值
     private String sdkAppId;
 
-    @Value("${sms.mathcalc.secretId}")
-    private String secretId;
-
-    @Value("${sms.mathcalc.secretKey}")
-    private String secretKey;
-
     @Value("${sms.mathcalc.signName}")
     private String signName;
 
-    @Value("${sms.mathcalc.templateCodeId}")
-    private String templateCodeId;
+    @Value("${sms.mathcalc.templateId}")
+    private String templateId;
 
     @Override
     public int sendCode(String phone, String code) {
-        return sendCode(sdkAppId, signName, phone, code, templateCodeId);
+        return sendCode(sdkAppId, signName, phone, code, templateId);
     }
 
 }
